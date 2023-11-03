c410008f7bf47aadd3cd5e712585a5d8d7546c1f
Danil Korotenko <danil.korotenko@gmail.com>
2023-11-03 12:40:53 +0200

    Initial Commit

---
+++ libproccpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  libproccpp
+//
+//  Created by Danil Korotenko on 11/3/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
